878748cbbb11ec83c05b6a2ee1061c7f20d0b477
KOEdoo <edookofi@gmail.com>
2022-09-30 10:56:10 +0000

    feat. add window for adding stock to product

---
+++ AddStock Module.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace InventoryManagementSystem
+{
+    partial class AddStock_Module
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddStock_Module));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBoxClose = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addStock = new System.Windows.Forms.NumericUpDown();
+            this.addButton = new System.Windows.Forms.Button();
+            this.productId = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.addStock)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(212)))));
+            this.panel1.Controls.Add(this.pictureBoxClose);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(414, 70);
+            this.panel1.TabIndex = 17;
+            // 
+            // pictureBoxClose
+            // 
+            this.pictureBoxClose.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxClose.Image")));
+            this.pictureBoxClose.Location = new System.Drawing.Point(390, -6);
+            this.pictureBoxClose.Name = "pictureBoxClose";
+            this.pictureBoxClose.Size = new System.Drawing.Size(28, 33);
+            this.pictureBoxClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxClose.TabIndex = 11;
+            this.pictureBoxClose.TabStop = false;
+            this.pictureBoxClose.Click += new System.EventHandler(this.pictureBoxClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("HP Simplified", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add Stock Module";
+            // 
+            // addStock
+            // 
+            this.addStock.Location = new System.Drawing.Point(68, 109);
+            this.addStock.Name = "addStock";
+            this.addStock.Size = new System.Drawing.Size(244, 22);
+            this.addStock.TabIndex = 18;
+            // 
+            // addButton
+            // 
+            this.addButton.BackColor = System.Drawing.Color.Green;
+            this.addButton.FlatAppearance.BorderSize = 0;
+            this.addButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addButton.Font = new System.Drawing.Font("HP Simplified", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addButton.ForeColor = System.Drawing.Color.White;
+            this.addButton.Location = new System.Drawing.Point(138, 154);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(86, 44);
+            this.addButton.TabIndex = 26;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = false;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // productId
+            // 
+            this.productId.AutoSize = true;
+            this.productId.Location = new System.Drawing.Point(12, 182);
+            this.productId.Name = "productId";
+            this.productId.Size = new System.Drawing.Size(63, 16);
+            this.productId.TabIndex = 27;
+            this.productId.Text = "productId";
+            this.productId.Visible = false;
+            // 
+            // AddStock_Module
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 234);
+            this.Controls.Add(this.productId);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.addStock);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AddStock_Module";
+            this.Text = "AddStock_Module";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.addStock)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBoxClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown addStock;
+        public System.Windows.Forms.Button addButton;
+        public System.Windows.Forms.Label productId;
+    }
+}
---
+++ AddStock Module.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InventoryManagementSystem.Src;
+
+namespace InventoryManagementSystem
+{
+    public partial class AddStock_Module : Form
+    {
+        public AddStock_Module()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBoxClose_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(productId.Text);
+            int stock = Convert.ToInt32(addStock.Value);
+            Admin.StockProduct(id, stock);
+            this.Dispose();
+        }
+    }
+}
