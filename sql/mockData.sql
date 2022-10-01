c3daa8009f7c7c22b98f04a3a84f18817e2d06e6
KOEdoo <edookofi@gmail.com>
2022-10-01 12:35:34 +0000

    refactor. include sql scripts

---
+++ sql/mockData.sql
@@ -0,0 +1,16 @@
+INSERT INTO category VALUES ("beverages")
+INSERT INTO category VALUES ("diary") 
+INSERT INTO category VALUES ("fruit")
+
+INSERT INTO product(barcode, name, categoryId, stock, costPrice, sellingPrice) VALUES ("0000000001","coffee",1,10,25,40)
+INSERT INTO product(barcode, name, categoryId, stock, costPrice, sellingPrice) VALUES ("0000000002","juice",1,7,10,17)
+INSERT INTO product(barcode, name, categoryId, stock, costPrice, sellingPrice) VALUES ("0000000003","soda",1,20,7,10)
+INSERT INTO product(barcode, name, categoryId, stock, costPrice, sellingPrice) VALUES ("0000000004","cheese",2,12,15,19.5)
+INSERT INTO product(barcode, name, categoryId, stock, costPrice, sellingPrice) VALUES ("0000000005","eggs",2,10,20,30)
+INSERT INTO product(barcode, name, categoryId, stock, costPrice, sellingPrice) VALUES ("0000000006","milk",2,30,9,11)
+INSERT INTO product(barcode, name, categoryId, stock, costPrice, sellingPrice) VALUES ("0000000007","yoghurt",2,12,5,12)
+INSERT INTO product(barcode, name, categoryId, stock, costPrice, sellingPrice) VALUES ("0000000008","butter",2,4,12,20)
+INSERT INTO product(barcode, name, categoryId, stock, costPrice, sellingPrice) VALUES ("0000000009","apple",3,10,3,5)
+INSERT INTO product(barcode, name, categoryId, stock, costPrice, sellingPrice) VALUES ("0000000010","orange",3,20,1,2)
+
+
---
+++ sql/setup.sql
@@ -0,0 +1,73 @@
+
+DROP DATABASE IF EXISTS LearningDb2;
+GO
+CREATE DATABASE LearningDb2;
+GO
+USE LearningDb2;
+GO
+
+DROP TABLE IF EXISTS [transaction];
+DROP TABLE IF EXISTS till;
+DROP TABLE IF EXISTS [user];
+DROP TABLE IF EXISTS transactionItem;
+DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS category;
+
+
+
+CREATE TABLE [user]
+(userId INT NOT NULL IDENTITY(1,1),
+name VARCHAR(50) NOT NULL,
+username VARCHAR(20) NOT NULL,
+role VARCHAR(10) NOT NULL CHECK (role IN('admin','attendant')),
+phoneNumber CHAR(10),
+password CHAR(64) NOT NULL,
+[access] VARCHAR(10) NOT NULL DEFAULT "granted" CHECK (access IN('granted','revoked','reset'))
+UNIQUE(username),
+PRIMARY KEY (userId));
+
+CREATE TABLE category
+(categoryId INT NOT NULL IDENTITY(1,1),
+name VARCHAR(25) NOT NULL,
+PRIMARY KEY(categoryId));
+
+CREATE TABLE product
+(productId INT NOT NULL IDENTITY(1,1),
+barcode VARCHAR(10) NOT NULL,
+name VARCHAR(30) NOT NULL,
+categoryId INT NOT NULL,
+stock INT NOT NULL,
+reorderLevel INT DEFAULT 0 ,
+costPrice MONEY NOT NULL,
+sellingPrice MONEY NOT NULL,
+PRIMARY KEY (productId),	
+UNIQUE(barcode),
+CONSTRAINT product_category FOREIGN KEY (categoryId) REFERENCES category (categoryId));
+
+CREATE TABLE till
+(tillId INT NOT NULL IDENTITY(1,1),
+[open] DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+[close] DATETIME,
+userId INT,
+PRIMARY KEY (tillId),
+CONSTRAINT attendant_till FOREIGN KEY (userId) REFERENCES [user] (userId));
+
+CREATE TABLE transactionItem
+(transactionItemId INT NOT NULL IDENTITY(1,1),
+productId INT,
+quantity INT,
+unitPrice MONEY NOT NULL,
+price MONEY NOT NULL,
+PRIMARY KEY (transactionItemId),
+CONSTRAINT transactionItem_product FOREIGN KEY (productId) REFERENCES product (productId));
+
+CREATE TABLE [transaction]
+(transactionId INT NOT NULL IDENTITY(1,1),
+transactionItem VARCHAR(max),
+time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+tillId INT,
+total MONEY,
+PRIMARY KEY (transactionId),
+CONSTRAINT transaction_till FOREIGN KEY (tillId) REFERENCES till (tillId));
+
+GO
